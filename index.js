cb186e07b839034240b9556f04da7f409b5725ac
Ayush <ayushjha090@gmail.com>
2022-06-10 15:15:23 +0530

    Added controller for adding todo details to the mongodb database.

--- index.js
+++ index.js
@@ -9,9 +9,25 @@ const TODO = require('./model/todo');
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static("assets"));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
 
 app.get('/', (req, res)=>{
     return res.render("home")
+});
+
+app.post('/add-to-list', (req, res)=>{
+    TODO.create({
+        description: req.body.description,
+        category: req.body.category,
+        date: req.body.date
+    }, (err, detail)=>{
+        if(err){
+            console.error.bind(console, "Error in creating todo!");
+        }
+        return res.redirect('back');
+    });
 });
 
 app.listen(port, (err)=>{

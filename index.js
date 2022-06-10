18c02f48e000117604c61421227fa9e301a07f7f
Ayush <ayushjha090@gmail.com>
2022-06-10 16:54:36 +0530

    Added views for showing todos in the home page and added controller for deleting taks from the list.

--- index.js
+++ index.js
@@ -9,14 +9,25 @@ const TODO = require('./model/todo');
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static("assets"));
+
+// Middleware for accessing req data
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-
+// Controller for home page
 app.get('/', (req, res)=>{
-    return res.render("home")
+    TODO.find({}, (err, todoDetails)=>{
+        if(err){
+            console.error.bind(console, "Error in finding todo details!");
+            return;
+        }
+        return res.render('home', {
+            details: todoDetails
+        });
+    });
 });
 
+// Controlller for accessing form data and storing it on the database
 app.post('/add-to-list', (req, res)=>{
     TODO.create({
         description: req.body.description,
@@ -25,6 +36,17 @@ app.post('/add-to-list', (req, res)=>{
     }, (err, detail)=>{
         if(err){
             console.error.bind(console, "Error in creating todo!");
+        }
+        return res.redirect('back');
+    });
+});
+
+// Controller for deleting entry from the database
+app.post('/delete-from-list', (req, res)=>{
+    TODO.findByIdAndDelete(req.body.id, (err)=>{
+        if(err){
+            console.error.bind(console, "Error in finding the detail!");
+            return;
         }
         return res.redirect('back');
     });
